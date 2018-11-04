2e2d884a8a480c396ef92f6b54c7378172030180
Valentin7arema <44587140+Valentin7arema@users.noreply.github.com>
2018-11-04 14:06:46 +0200

    .
    .

---
+++ Lesson 2/Assets/Scripts/PlayerControll.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Text;
+
+
+public class PlayerControll : MonoBehaviour {
+    public float speed = 6.0f;
+    public float jumpSpeed = 4.0f;
+    public float gravity = 20.0f;
+    public float rotationSpeed = 90;
+    public Text score;
+    public GameObject coin;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private Vector3 moveCoin;
+    private CharacterController controller;
+    private int count=0;
+    private bool DoubleJump=false;
+    void Start() {
+        controller = gameObject.GetComponent<CharacterController>();
+        score.text = count.ToString();
+    }
+    void Update() {
+        if (controller.isGrounded) {
+            moveDirection = new Vector3(0.0f, 0.0f, Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection *=  speed;
+            if (Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed;
+                DoubleJump = true;
+            }
+            
+        } else {
+            if (DoubleJump && Input.GetButtonDown("Jump"))
+            {
+                moveDirection.y = jumpSpeed * 2;
+                DoubleJump = false;
+            }
+            moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+        }
+        controller.Move(moveDirection * Time.deltaTime);
+        Vector3 rotateCoords = new Vector3(0, Input.GetAxis("Horizontal") * rotationSpeed * Time.deltaTime, 0);
+        controller.transform.Rotate(rotateCoords);
+        
+        
+
+    }
+    void SetCountText()
+    {
+        count++;
+        score.text =count.ToString();
+       
+    }
+
+    public void OnControllerColliderHit(ControllerColliderHit hit) {
+        if (hit.gameObject.tag == "Coin") {
+            
+            Destroy(hit.gameObject);
+            
+            moveCoin = new Vector3(Random.Range(-50,50), Random.Range(0, 5), Random.Range(-150, 150));
+            Debug.Log(moveCoin);
+            GameObject gm = Instantiate(coin);
+            gm.transform.position = moveCoin;
+            SetCountText();
+
+            
+        }
+    }
+}
---
+++ Lesson 3/Assets/Scripts/GameManager.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Image playerOne;
+    public Image playerTwo;
+    public Sprite[] playerSprites;
+    public Text manageText;
+
+    private int gameStage = 0;
+    private float timer = 0;
+    private float bangTimer = 0;
+    private bool isPlayerOneShot = false;
+    private bool isPlayerTwoShot = false;
+
+
+    private bool _isComputer = false;
+    private float compShot = 0.5f;
+
+    private int recordOne = 0;
+    private int recordTwo = 0;
+    private int maxRounds = 10;
+
+    public InputField _input;
+    public GameObject _panel;
+    public GameObject StartButton;
+    public GameObject ExitButton;
+    public Text recOne;
+    public Text recTwo;
+
+    public GameObject PlayerTwoShotButton;
+
+    public void StartGame() {
+        manageText.text = "ready";
+        playerOne.sprite = playerSprites[1];
+        playerTwo.sprite = playerSprites[1];
+        gameStage = 1;
+        compShot = UnityEngine.Random.Range(0.5f, 1f);
+        if (_isComputer) PlayerTwoShotButton.SetActive(false);
+        maxRounds = Convert.ToInt32(_input.text);
+    }
+
+    public void Update() {
+        recOne.text = "Player One: " + recordOne.ToString();
+        recTwo.text = "Player Two: " + recordTwo.ToString();
+        if (gameStage == 1) {
+            if (timer >= 1.0f) {
+                manageText.text = "steady";
+                bangTimer = UnityEngine.Random.Range(1.0f, 3.0f);
+                gameStage = 2;
+                timer = 0;
+            } else {
+                timer += Time.deltaTime;
+            }
+        } else if (gameStage == 2) {
+            if (timer >= bangTimer) {
+                manageText.text = "!bang!";
+                bangTimer = 0;
+                gameStage = 3;
+                timer = 0;
+            } else {
+                timer += Time.deltaTime;
+            }
+        }
+
+        if (gameStage == 3 && _isComputer)
+        {
+            compShot -= Time.deltaTime;
+            if (compShot <= 0) Shot(2);
+        }
+
+        if (maxRounds==recordOne || maxRounds == recordTwo)
+        {
+            recordOne = 0;
+            recordTwo = 0;
+            StartButton.SetActive(false);
+            _panel.SetActive(true);
+        }
+
+    }
+
+    public void Shot(int player) {
+        if (gameStage == 0) return;
+            Image activePlayer = (player == 1) ? playerOne : playerTwo;
+        if (gameStage == 3) {
+            Image enemyPlayer = (player == 2) ? playerOne : playerTwo;
+            activePlayer.sprite = playerSprites[2];
+            enemyPlayer.sprite = playerSprites[3];
+            manageText.text = "player " + player + " win!";
+            if (player == 1) { recordOne++; } else { recordTwo++; }
+
+            gameStage = 0;
+            isPlayerOneShot = false;
+            isPlayerTwoShot = false;
+        } else {
+            if (player == 1) isPlayerOneShot = true;
+            if (player == 2) isPlayerTwoShot = true;
+            if (isPlayerOneShot && isPlayerTwoShot) {
+                manageText.text = "Too fast, guys!";
+                bangTimer = 0;
+                gameStage = 0;
+                timer = 0;
+                isPlayerOneShot = false;
+                isPlayerTwoShot = false;
+            }
+            activePlayer.sprite = playerSprites[4];
+        }
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Computer()
+    {
+        _isComputer = true;
+    }
+
+    public void DeComputer()
+    {
+        _isComputer = false;
+    }
+}
